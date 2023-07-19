ce5a1dd7a4f44b8e2983867f1213f268f27ce426
unimafy <nima.yonten1729@gmail.com>
2023-07-19 19:53:32 +0600

    initial commit

---
+++ spec/rulit/core_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rulit::Core do
+  it "has a version number" do
+    expect(Rulit::Core::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
